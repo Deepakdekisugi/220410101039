bf7fd7c542cf669fbf306148183cc9ef2ee98c55
Deepak <dk376907@gmail.com>
2025-03-21 11:35:03 +0530

    created feed page ui

---
+++ Question_1/src/pages/Feed.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { fetchPosts } from '../api';
+import PostCard from '../components/PostCard';
+import Loader from '../components/Loader';
+import { Container, Grid } from '@mui/material';
+
+const Feed = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await fetchPosts();
+            setPosts(data);
+            setLoading(false);
+        };
+
+        getPosts();
+
+        const interval = setInterval(() => {
+            getPosts(); 
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <Container>
+            {loading ? (
+                <Loader />
+            ) : (
+                <Grid container spacing={2}>
+                    {posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map(post => (
+                        <Grid item xs={12} sm={6} md={4} key={post.id}>
+                            <PostCard post={post} />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
+        </Container>
+    );
+};
+
+export default Feed;
