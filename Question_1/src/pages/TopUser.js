6d50202c0a9dbeda0e94f5b2220e48da961cc1c8
Deepak <dk376907@gmail.com>
2025-03-21 11:29:34 +0530

    loaded top users here

---
+++ Question_1/src/pages/TopUser.js
@@ -0,0 +1,39 @@
+// src/pages/TopUsers.js
+import React, { useEffect, useState } from 'react';
+import { fetchUsers } from '../api';
+import UserCard from '../components/UserCard';
+import Loader from '../components/Loader';
+import { Container, Grid } from '@mui/material';
+
+const TopUsers = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await fetchUsers();
+            const sortedUsers = data.sort((a, b) => b.postCount - a.postCount).slice(0, 5);
+            setUsers(sortedUsers);
+            setLoading(false);
+        };
+        getUsers();
+    }, []);
+
+    return (
+        <Container>
+            {loading ? (
+                <Loader />
+            ) : (
+                <Grid container spacing={2}>
+                    {users.map(user => (
+                        <Grid item xs={12} sm={6} md={4} key={user.id}>
+                            <User Card user={user} />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
+        </Container>
+    );
+};
+
+export default TopUsers;
