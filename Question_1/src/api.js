996a2b4330758415e32a165ce568e9e9b7bf8a2c
Deepak <dk376907@gmail.com>
2025-03-21 11:11:32 +0530

    wrote functions for fetching post as well as users

--- Question_1/src/api.js
+++ Question_1/src/api.js
@@ -2,3 +2,12 @@ import axios from 'axios';
 
 const API_URL = 'http://20.244.56.144/test/';
 
+export const fPosts = async () => {
+    const response = await axios.get(`${API_URL}/posts`);
+    return response.data;
+};
+
+export const fUsers = async () => {
+    const response = await axios.get(`${API_URL}/users`);
+    return response.data;
+};
