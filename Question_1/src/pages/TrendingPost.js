32fe912cc0712730565a318097ecb9c5a048f347
Deepak <dk376907@gmail.com>
2025-03-21 11:33:22 +0530

    created trending post ui

---
+++ Question_1/src/pages/TrendingPost.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { fetchPosts } from '../api';
+import PostCard from '../components/PostCard';
+import Loader from '../components/Loader';
+import { Container, Grid } from '@mui/material';
+
+const TrendingPosts = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await fetchPosts();
+            const maxComments = Math.max(...data.map(post => post.comments.length));
+            const trendingPosts = data.filter(post => post.comments.length === maxComments);
+            setPosts(trendingPosts);
+            setLoading(false);
+        };
+        getPosts();
+    }, []);
+
+    return (
+        <Container>
+            {loading ? (
+                <Loader />
+            ) : (
+                <Grid container spacing={2}>
+                    {posts.map(post => (
+                        <Grid item xs={12} sm={6} md={4} key={post.id}>
+                            <PostCard post={post} />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
+        </Container>
+    );
+};
+
+export default TrendingPosts;
